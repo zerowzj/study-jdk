a8b474b8ed2aeb06813a20a01bda4a74beb1bd8f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-07 18:07:51 +0800

    更新代码

---
+++ study-reflect/src/main/java/study/reflect/clazz/Clazz_Test.java
@@ -0,0 +1,29 @@
+package study.reflect.clazz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class Clazz_Test {
+
+    @Test
+    public void get_test() {
+        Class clazz = Object.class;
+        int i = 0x11;
+        log.info("{}", i);
+    }
+
+    @Test
+    public void getClass_test() {
+        Class clazz = this.getClass();
+        int i = 0x11;
+        log.info("{}", i);
+    }
+
+    @Test
+    public void getClass1_test() {
+        Class clazz = this.getClass();
+        int i = 0x11;
+        log.info("{}", i);
+    }
+}
