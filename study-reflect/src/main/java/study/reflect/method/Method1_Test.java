15af5b7676d1754b2cc6da13027ebcba92e310bd
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-08 13:29:48 +0800

    更新代码

---
+++ study-reflect/src/main/java/study/reflect/method/Method1_Test.java
@@ -0,0 +1,41 @@
+package study.reflect.method;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import study.reflect.MyBean;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class Method1_Test {
+
+
+    @Test
+    public void getName_test() {
+        Method[] methods = MyBean.class.getDeclaredMethods();
+        for (Method method : methods) {
+            log.info("{}", method.getName());
+        }
+    }
+
+    @Test
+    public void getReturnType_test() {
+        Method[] methods = MyBean.class.getDeclaredMethods();
+        for (Method method : methods) {
+            log.info("{} {}()", method.getReturnType().getSimpleName(), method.getName());
+        }
+    }
+
+
+    @Test
+    public void getParameters_test() {
+        Method[] methods = MyBean.class.getDeclaredMethods();
+        for (Method method : methods) {
+            log.info("{}: ", method.getName());
+            Class[] clazzs = method.getParameterTypes();
+            for (Class clazz : clazzs) {
+                log.info("===>{}", clazz.getName());
+            }
+        }
+    }
+}
--- study-reflect/src/main/java/study/reflect/method/Method2_Test.java
+++ study-reflect/src/main/java/study/reflect/method/Method2_Test.java
@@ -2,7 +2,7 @@ package study.reflect.method;
 
 import java.lang.reflect.Method;
 
-public class Method_Test {
+public class Method2_Test {
 
     public void test() {
         Method method;
