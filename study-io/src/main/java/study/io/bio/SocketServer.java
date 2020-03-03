cb1a5d25e5c450b49d41e13850e0ca031dfee7bd
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-03 16:00:23 +0800

    更新代码

--- study-io/src/main/java/study/io/bio/SocketServer.java
+++ study-io/src/main/java/study/io/bio/SocketServer.java
@@ -1,23 +1,31 @@
 package study.io.bio;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+@Slf4j
 public class SocketServer {
 
     private static ServerSocket server;
 
     public static void main(String[] args) {
         try {
-            server = new ServerSocket();
+            server = new ServerSocket(8080);
             Socket socket = server.accept();
 
             InputStream is = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            OutputStream os = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(os);
-
+            String msg = reader.readLine();
+            log.info("===> {}", msg);
+            while(true){
+                OutputStream os = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(os);
+                writer.write(String.format("get client msg: {}", msg));
+                writer.flush();
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
