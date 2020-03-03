bb296277d8e76ba3e812cd32132d34acfee92350
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-03 21:45:15 +0800

    更新代码

--- study-io/src/main/java/study/io/bio/SocketServer.java
+++ study-io/src/main/java/study/io/bio/SocketServer.java
@@ -10,15 +10,16 @@ import java.net.Socket;
 @Slf4j
 public class SocketServer {
 
+    private static final int PORT = 8800;
+
     private static ServerSocket server;
 
     public static void main(String[] args) {
         try {
-            InetSocketAddress address = new InetSocketAddress("127.0.0.1", 80);
-            server = new ServerSocket();
-            server.bind(address);
+            server = new ServerSocket(PORT);
             Socket socket = server.accept();
 
+            log.info("fsafsdafsdafsafd");
             InputStream is = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String msg = reader.readLine();
@@ -28,6 +29,9 @@ public class SocketServer {
                 PrintWriter writer = new PrintWriter(os);
                 writer.write(String.format("get client msg: {}", msg));
                 writer.flush();
+
+                writer.close();
+                os.close();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
