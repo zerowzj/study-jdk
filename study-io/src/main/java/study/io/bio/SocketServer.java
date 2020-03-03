9ca21a2188ea40ee23d92a6f0832f15f1311371a
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-03 21:38:57 +0800

    更新代码

--- study-io/src/main/java/study/io/bio/SocketServer.java
+++ study-io/src/main/java/study/io/bio/SocketServer.java
@@ -3,6 +3,7 @@ package study.io.bio;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,14 +14,16 @@ public class SocketServer {
 
     public static void main(String[] args) {
         try {
-            server = new ServerSocket(8080);
+            InetSocketAddress address = new InetSocketAddress("127.0.0.1", 80);
+            server = new ServerSocket();
+            server.bind(address);
             Socket socket = server.accept();
 
             InputStream is = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String msg = reader.readLine();
             log.info("===> {}", msg);
-            while(true){
+            while (true) {
                 OutputStream os = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(os);
                 writer.write(String.format("get client msg: {}", msg));
