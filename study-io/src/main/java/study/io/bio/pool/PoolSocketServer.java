b8f62991eccbe7f5c96e4d4f5e8811c76a343e1d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-03 22:18:29 +0800

    更新代码

--- study-io/src/main/java/study/io/bio/pool/PoolSocketServer.java
+++ study-io/src/main/java/study/io/bio/pool/PoolSocketServer.java
@@ -1,4 +1,4 @@
-package study.io.bio;
+package study.io.bio.pool;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -7,11 +7,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 @Slf4j
-public class SocketServer {
+public class PoolSocketServer {
 
     private static final int PORT = 8800;
 
     private static ServerSocket server;
+
+    public void start() {
+        try {
+            server = new ServerSocket(PORT);
+            server.accept();
+        } catch (Exception ex) {
+        }
+    }
 
     public static void main(String[] args) {
         try {
@@ -22,17 +30,17 @@ public class SocketServer {
             log.info("get request");
             InputStream is = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String msg = reader.readLine();
-            log.info("===> {}", msg);
-//            while (!((msg = reader.readLine()) == null)) {
-            OutputStream os = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(os);
-            writer.write(String.format("get client msg: %s", msg));
-            writer.flush();
+            String msg;
+            while (!((msg = reader.readLine()) == null)) {
+                log.info("===> {}", msg);
+                OutputStream os = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(os);
+                writer.write(String.format("get client msg: %s", msg));
+                writer.flush();
 
-            writer.close();
-            os.close();
-//            }
+                writer.close();
+                os.close();
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
