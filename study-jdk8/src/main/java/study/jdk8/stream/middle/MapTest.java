3d1fafb22eb9c4d68c25de4a83c715242cd27f6c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-13 14:40:11 +0800

    更新代码

--- study-jdk8/src/main/java/study/jdk8/stream/middle/MapTest.java
+++ study-jdk8/src/main/java/study/jdk8/stream/middle/MapTest.java
@@ -1,4 +1,27 @@
 package study.jdk8.stream.middle;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * map 操作符要求输入一个Function的函数是接口实例
+ * 功能是将T类型转换成R类型的
+ */
+@Slf4j
 public class MapTest {
+
+    @Test
+    public void test() {
+        List<Integer> lenLt = new ArrayList();
+
+        Stream.of("apple", "banana", "orange", "waltermaleon", "grape")
+                .map(e -> e.length())
+                .forEach(e -> lenLt.add(e));
+
+        log.info("{}", lenLt);
+    }
 }
