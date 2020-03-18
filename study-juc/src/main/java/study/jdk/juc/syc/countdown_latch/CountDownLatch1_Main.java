31d5f6037d2c4ed08dae0517a71bf8c3568f3e18
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-18 10:46:33 +0800

    更新代码

--- study-juc/src/main/java/study/jdk/juc/syc/countdown_latch/CountDownLatch1_Main.java
+++ study-juc/src/main/java/study/jdk/juc/syc/countdown_latch/CountDownLatch1_Main.java
@@ -1,6 +1,7 @@
 package study.jdk.juc.syc.countdown_latch;
 
 import lombok.extern.slf4j.Slf4j;
+import study.jdk.juc.Randoms;
 import study.jdk.juc.Sleeps;
 
 import java.util.concurrent.CountDownLatch;
@@ -14,21 +15,30 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatch1_Main {
 
     public static void main(String[] args) {
+        //（★）
         CountDownLatch latch = new CountDownLatch(2);
         Thread t1 = new Thread(() -> {
-            Sleeps.seconds(2);
-            log.info("i am t1 thread");
+            //业务逻辑
+            int sec = Randoms.nextInt(10);
+            log.info("i am t1 thread, sleep {}s", sec);
+            Sleeps.seconds(sec);
+            log.info("i am t1 thread, finish");
+            //（★）
             latch.countDown();
         });
         Thread t2 = new Thread(() -> {
-            Sleeps.seconds(2);
-            log.info("i am t2 thread");
+            int sec = Randoms.nextInt(10);
+            log.info("i am t2 thread, sleep {}s", sec);
+            Sleeps.seconds(sec);
+            log.info("i am t2 thread, finish");
+            //（★）
             latch.countDown();
         });
         t1.start();
         t2.start();
 
         try {
+            //（★）集合点
             latch.await();
         } catch (InterruptedException ex) {
             ex.printStackTrace();
